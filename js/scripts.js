5917801338889c7a048d2510ce05e549b397c2f1
stwilliams0223 <williamsfamily@Williams-Family.local>
2015-04-13 12:27:09 -0700

    audio div

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,50 @@ $('#audio-filter').click(function(){
   $('.mejs-audio').show().addclass('pulse');
   
 });
+
+
+  $(function(){
+        $('video,audio').mediaelementplayer({
+            success: function (mediaElement, domObject) {
+                   var audio_src = $("li.current").attr("data-url");
+              mediaElement.setSrc(audio_src);
+                mediaElement.addEventListener('ended', function (e) {
+                    mejsPlayNext(e.target);
+                }, false);
+              
+            },
+            keyActions: []
+        });
+
+        $('.mejs-list li').click(function() {
+            $(this).addClass('current').siblings().removeClass('current');
+            var audio_src = $(this).attr("data-url");
+            $('audio#mejs:first').each(function(){
+                this.player.pause();
+                this.player.setSrc(audio_src);
+                this.player.play();
+            });
+        });
+
+    });
+
+    function mejsPlayNext(currentPlayer) {
+        if ($('.mejs-list li.current').length > 0){ // get the .current song
+            var current_item = $('.mejs-list li.current:first'); // :first is added if we have few .current classes
+            var audio_src = $(current_item).next().text();
+            $(current_item).next().addClass('current').siblings().removeClass('current');
+            console.log('if '+audio_src);
+        }else{ // if there is no .current class
+            var current_item = $('.mejs-list li:first'); // get :first if we don't have .current class
+            var audio_src = $(current_item).next().text();
+            $(current_item).next().addClass('current').siblings().removeClass('current');
+            console.log('elseif '+audio_src);
+        }
+
+        if( $(current_item).is(':last-child') ) { // if it is last - stop playing
+            $(current_item).removeClass('current');
+        }else{
+            currentPlayer.setSrc(audio_src);
+            currentPlayer.play();
+        }
+    }
